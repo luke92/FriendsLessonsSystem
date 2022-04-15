e37f9d153741d5c7aebd33d743fe8a81787d3235
Lucas Vargas <lucasjv92@gmail.com>
2022-04-15 14:48:51 -0300

    Create fake endpoints

--- BackEnd/web-service-gin/main.go
+++ BackEnd/web-service-gin/main.go
@@ -41,10 +41,45 @@ func getUserById(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
 }
 
+func getFriendships(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func getFriendsByUserId(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop through the list of users, looking for
+	// an user whose ID value matches the parameter.
+	for _, a := range users {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+}
+
+func getLessonsByUserId(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop through the list of users, looking for
+	// an user whose ID value matches the parameter.
+	for _, a := range users {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/users", getUsers)
 	router.GET("/users/:id", getUserById)
+	router.GET("/friendships", getFriendships)
+	router.GET("/users/:id/friends", getFriendsByUserId)
+	router.GET("/users/:id/lessons", getLessonsByUserId)
 
 	router.Run("localhost:8080")
 }
