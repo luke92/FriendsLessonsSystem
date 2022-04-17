5da63ebf5a3e93a350ae6f5824b2504956bf249f
Lucas Vargas <lucasjv92@gmail.com>
2022-04-17 20:05:33 -0300

    Configure port

--- BackEnd/web-service-gin/main.go
+++ BackEnd/web-service-gin/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
+
 	di "github.com/luke92/FriendsLessonsSystem/di"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	server, diErr := di.InitializeAPI()
 	if diErr != nil {
 		log.Fatal("cannot start server: ", diErr)
 	} else {
+		log.Println("listening on", port)
 		server.Start()
 	}
 }
